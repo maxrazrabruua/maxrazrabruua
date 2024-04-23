49253f5b7058f34f07908b1312a5bff665c77782
maxrazrabruua <149672990+maxrazrabruua@users.noreply.github.com>
2024-04-23 11:13:42 +0300

    InstallersForSystem.py

---
+++ InstallersForSystem.py
@@ -0,0 +1,15 @@
+import requests
+
+def download(file_url, destination_path):
+    try:
+        # Отправка GET-запроса для загрузки файла
+        response = requests.get(file_url, stream=True)
+        # Запись содержимого файла в локальный файл
+        with open(destination_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=128):
+                f.write(chunk)
+        print("d:/" + "Файл успешно загружен в", destination_path)
+        return True
+    except Exception as e:
+        print("d:/" + "Ошибка при загрузке файла:", e)
+        return False
